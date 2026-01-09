12a21f1b6e041cb96c49a630dddf38bf00439f4c
David P. Chassin <david.chassin@me.com>
2026-01-09 10:35:39 -0800

    Update cli.py

--- weather/cli.py
+++ weather/cli.py
@@ -1,5 +1,38 @@
 """Weather accessor CLI
 
+# Syntax
+    
+    weather [-h] [-S STATE] [-C COUNTY] [-Y YEAR] [-o OUTPUT] [-w] [-d] {viewer,print,plot}
+
+# Commands
+
+  - `viewer`: open Marimo notebook
+
+  - `print`: output data
+  
+  - `plot`: output plot
+
+# Options
+  
+  `-h|--help`: show this help message and exit
+  
+  `-S|--state STATE`: state abbreviation of county
+  
+  `-C|--county COUNTY`: county name of weather data to collect
+  
+  `-Y|--year YEAR`: year of weather data to collect
+  
+  `-o|--output OUTPUT`: output file name
+
+  `-w|--warning`: enable warning messages from python
+
+  `-d|--debug`: enable debug traceback on exceptions
+
+# Description
+
+Weather data command-line interface.
+
+See https://www.eudoxys.com/weather for documentation.
 """
 
 import os
@@ -8,6 +41,7 @@ import argparse
 import warnings
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from weather import Weather
 
@@ -46,14 +80,19 @@ def main(*args:list[str],**kwargs:dict[str,str]) -> int:
             epilog="See https://www.eudoxys.com/weather for documentation. ",
             )
 
+        parser.add_argument("command",
+            choices={"print","plot","viewer"},
+            help="Weather command")
+
         parser.add_argument("-S","--state",
-            required=True,
             type=str,
             help="state abbreviation of county")
         parser.add_argument("-C","--county",
-            required=True,
             type=str,
             help="county name of weather data to collect")
+        parser.add_argument("-V","--variable",
+            type=str,
+            help="variable to output")
         parser.add_argument("-Y","--year",
             type=int,
             default=None,
@@ -79,28 +118,61 @@ def main(*args:list[str],**kwargs:dict[str,str]) -> int:
                 file=sys.stderr,
                 )
 
-        data = Weather(args.state,args.county,args.year)
+        if args.command in {"print","plot"}:
+            assert not args.state is None, f"state is required"
+            assert not args.county is None, f"county is required"
+            data = Weather(args.state,args.county,args.year)
 
-        if args.output is None:
+        match args.command:
+            
+            case "print":
+            
+                if args.output is None:
 
-            pd.options.display.max_rows=None
-            pd.options.display.max_columns=None
-            pd.options.display.width=None
+                    pd.options.display.max_rows=None
+                    pd.options.display.max_columns=None
+                    pd.options.display.width=None
 
-            print(data)
-            return E_OK
+                    print(data)
+                    return E_OK
 
-        if args.output.endswith(".csv"):
+                if args.output.endswith(".csv"):
 
-            data.to_csv(args.output,index=True,header=True)
-            return E_OK
+                    data.to_csv(args.output,index=True,header=True)
+                    return E_OK
 
-        if args.output.endswith(".csv.gz"):
+                if args.output.endswith(".csv.gz"):
 
-            data.to_csv(args.output,index=True,header=True,compression="gzip")
-            return E_OK
+                    data.to_csv(args.output,index=True,header=True,compression="gzip")
+                    return E_OK
 
-        raise ValueError(f"output={args.output} is not valid")
+                raise ValueError(f"output={args.output} is not valid")
+
+            case "plot":
+
+                plt.figure(figsize=(20,10))
+                data[args.variable.split(",") if args.variable else data.columns].plot(
+                    ax=plt.gca(),
+                    grid=True,
+                    legend=True,
+                    xlabel="Date/Time",
+                    ylabel="Value",
+                    title=f"{args.county} {args.state}"
+                    )
+
+                if args.output is None:
+
+                    plt.show()
+                    return E_OK
+
+                else:
+
+                    plt.savefig(args.output)
+                    return E_OK
+
+            case "viewer":
+
+                return os.system(f"marimo run {os.path.dirname(__file__)}/viewer.py")
 
     # pylint: disable=broad-exception-caught
     except Exception as err:
@@ -112,4 +184,4 @@ def main(*args:list[str],**kwargs:dict[str,str]) -> int:
         return E_FAILED
 
 if __name__ == "__main__":
-    main("--state","CA","--county","Alameda")
+    main("plot","--state","AZ","--county","Maricopa","-V","temperature_degF")
